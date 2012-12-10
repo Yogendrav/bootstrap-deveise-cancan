817e6d954391f62909184555faa6aba4e78c8cfa
ntf <ntf@linux.local>
2012-12-10 16:04:43 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapDeviseCancan::Application.config.secret_token = 'd140f036d3439205ea242fef77182e3ac08e3156cfd7cdee913fd559a35f420207d0feea7acf08ab4517d5b98c64616c280e06a2ba557f8da076a1e9dc98dd0c'
